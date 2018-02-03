64aefecdbe472dccbe527422267ed8c2fe866711
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-03 09:43:31 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num =0;
+	do{
+
+	    num ++;
+        System.out.println( "7  * " + num + "=" + 7 * num );
+
+    }
+    while( num<10);
+
+    }
+}
